55b8a870dc9cd3568fe3f6bf469aa931c6781ed5
trindadeeesx <trindadesx@proton.me>
2026-01-06 21:15:21 -0300

    add color for terminal

--- ui/style.py
+++ ui/style.py
@@ -1,9 +1,11 @@
 class C:
     RESET = "\033[0m"
-
     USER = "\033[1;36m"  # ciano
     DOMINUS = "\033[1;31m"  # vermelho
     LUCIA = "\033[1;35m"  # roxo
     SYSTEM = "\033[1;90m"  # cinza
-
     PROMPT = "\033[1;32m"  # verde
+    SUCCESS = "\033[1;32m"  # verde brilhante
+    WARNING = "\033[1;33m"  # amarelo
+    ERROR = "\033[1;31m"  # vermelho brilhante
+    INFO = "\033[1;34m"  # azul
